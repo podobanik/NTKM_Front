f1288157df9dadcfd3e0503730c81cae4b643ee4
adminlocal <adminlocal@server.esdu.ru>
2024-08-20 10:52:01 +0300

    новый файл:    build/asset-manifest.json 	новый файл:    build/favicon.ico 	новый файл:    build/index.html 	новый файл:    build/logo192.png 	новый файл:    build/logo512.png 	новый файл:    build/manifest.json 	новый файл:    build/robots.txt новый файл:    build/static/css/main.e106a6f8.css 	новый файл:    build/static/css/main.e106a6f8.css.map 	новый файл:    build/static/js/main.b9a3fa3e.js 	новый файл:    build/static/js/main.b9a3fa3e.js.LICENSE.txt 	новый файл:    build/static/js/main.b9a3fa3e.js.map 	изменено:      package-lock.json 	изменено:      package.json 	изменено:      src/App.js 	изменено:      src/index.css 	изменено:      src/index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,196 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+  baseURL: "http://127.0.0.1:8000"
+});
+
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState();
+  const [registrationToggle, setRegistrationToggle] = useState(false);
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [second_name, setSecondName] = useState('');
+  const [title, setTitle] = useState('');
+  
+  useEffect(() => {
+    client.get("/api/user")
+    .then(function(res) {
+      setCurrentUser(true);
+    })
+    .catch(function(error) {
+      setCurrentUser(false);
+    });
+  }, []);
+
+  function update_form_btn() {
+    if (registrationToggle) {
+      document.getElementById("form_btn").innerHTML = "Регистрация";
+      setRegistrationToggle(false);
+    } else {
+      document.getElementById("form_btn").innerHTML = "Войти";
+      setRegistrationToggle(true);
+    }
+  }
+
+  function submitRegistration(e) {
+    e.preventDefault();
+    client.post(
+      "/api/register",
+      {
+        email: email,
+        username: username,
+        password: password
+      }
+    ).then(function(res) {
+      client.post(
+        "/api/login",
+        {
+          email: email,
+          password: password
+        }
+      ).then(function(res) {
+        setCurrentUser(true);
+      });
+    });
+  }
+
+  function submitLogin(e) {
+    e.preventDefault();
+    client.post(
+      "/api/login",
+      {
+        email: email,
+        password: password
+      }
+    ).then(function(res) {
+      setCurrentUser(true);
+    });
+  }
+
+  function submitLogout(e) {
+    e.preventDefault();
+    client.post(
+      "/api/logout",
+      {withCredentials: true}
+    ).then(function(res) {
+      setCurrentUser(false);
+    });
+  }
+  
+  
+  if (currentUser) {
+    return (
+      <div>
+        <Navbar bg="dark" variant='dark'>
+          <Container>
+            <Navbar.Brand>Аутентификация</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className='justify-content-end'>
+              <Navbar.Text>
+                <form onSubmit={e => submitLogout(e)}>
+                  <Button type="submit" variant='light'>Выход</Button>
+                </form>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+          <div className='center'>
+            <h2>Успешно!</h2>
+          </div>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar bg="dark" variant='dark'>
+      <Container>
+        <Navbar.Brand>Аутентификация</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className='justify-content-end'>
+          <Navbar.Text>
+            <Button id="form_btn" onClick={update_form_btn} variant ="light">Регистрация</Button>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    {
+      registrationToggle ? (
+        <div className="center">
+          <Form onSubmit={e => submitRegistration(e)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Адрес электронной почты:</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Логин:</Form.Label>
+              <Form.Control type="text" placeholder="Введите логин" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Пароль:</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicLastName">
+              <Form.Label>Фамилия сотрудника:</Form.Label>
+              <Form.Control type="text" placeholder="Введите фамилию" value={last_name} onChange={e => setLastName(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicFirstName">
+              <Form.Label>Имя сотрудника:</Form.Label>
+              <Form.Control type="text" placeholder="Введите имя" value={first_name} onChange={e => setFirstName(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicSecondName">
+              <Form.Label>Отчество сотрудника:</Form.Label>
+              <Form.Control type="text" placeholder="Введите отчество" value={second_name} onChange={e => setSecondName(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicTitle">
+              <Form.Label>Должность сотрудника:</Form.Label>
+              <Form.Control type="text" placeholder="Введите должность" value={title} onChange={e => setTitle(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Подтвердить
+            </Button>
+          </Form>
+        </div>
+      ) : (
+        <div className="center">
+          <Form onSubmit={e => submitLogin(e)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Адрес электронной почты:</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Пароль:</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Подтвердить
+            </Button>
+          </Form>
+        </div>
+      )
+    }
     </div>
   );
 }
